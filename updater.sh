9aa8b8b87cc804b8f962f1f0856f323daf6bd5d4
y-du <42994541+y-du@users.noreply.github.com>
2018-07-07 16:56:11 +0200

    working implementation

--- updater.sh
+++ updater.sh
@@ -14,8 +14,9 @@ log() {
     done
 }
 
-echo "************* starting auto update *************" | log
-echo "scanning for gateways ..." | log
+reboot_flag=0
+
+echo "************* starting gateway-updater 0.5 *************" | log
 
 for dir in $(cd .. && ls -d */); do
     path=$(dirname "$(pwd)")/${dir%/}
@@ -25,35 +26,56 @@ for dir in $(cd .. && ls -d */); do
         if ! [[ $update_result = *"fatal"* ]] || ! [[ $update_result = *"error"* ]]; then
             status_result=$(cd "$path" && git status)
             if [[ $status_result = *"behind"* ]]; then
-                echo "(${dir%/}) updates found!" | log
                 echo "(${dir%/}) downloading updates ..." | log
                 pull_result=$(cd "$path" && git pull 3>&1 1>&2 2>&3 >/dev/null)
                 if ! [[ $pull_result = *"fatal"* ]] || ! [[ $pull_result = *"error"* ]]; then
                     echo "(${dir%/}) applying updates ..." | log
                     commit_result=$(cd "$path" && git commit -a -m "update" 3>&1 1>&2 2>&3 >/dev/null)
                     if ! [[ $commit_result = *"fatal"* ]] || ! [[ $commit_result = *"error"* ]]; then
-                        echo "(${dir%/}) checking dependencies ..." | log
-                        if ! [ -z "$path/$task_file" ]; then
-                            for dep in $(cat "$path/$task_file"); do
-                                echo "$dep"
-                            done
-                        fi
+                        echo "(${dir%/}) update success" | log
+                        reboot_flag=1
                     else
                         echo "(${dir%/}) $commit_result" | log
                     fi
                 else
                    echo "(${dir%/}) $pull_result" | log
                 fi
-
-                #echo "rebooting in 30s ..." | log
-                #sleep 30
-                #sudo reboot
-            else
-                echo "(${dir%/}) up to date" | log
+            #else
+            #    echo "(${dir%/}) up to date" | log
+            fi
+            echo "(${dir%/}) checking dependencies ..." | log
+            if ! [ -z "$path/$task_file" ]; then
+                while IFS="," read -r pkg new_ver; do
+                    cur_ver=$(~/.pyenv/versions/sm_gateway/bin/python -m pip show $pkg | grep Version)
+                    if ! [[ $cur_ver = *"$new_ver"* ]]; then
+                        echo "(${dir%/}) '$pkg' -> $new_ver" | log
+                        if [[ $pkg = *"sepl-connector-client"* ]]; then
+                            rm_result=$(~/.pyenv/versions/sm_gateway/bin/python -m pip uninstall -y $pkg)
+                            inst_result=$(~/.pyenv/versions/sm_gateway/bin/python -m pip install git+ssh://git@gitlab.wifa.uni-leipzig.de/fg-seits/connector-client.git)
+                        else
+                            inst_result=$(~/.pyenv/versions/sm_gateway/bin/python -m pip install --upgrade $pkg==$new_ver)
+                        fi
+                        if [[ $inst_result = *"Success"* ]]; then
+                            echo "(${dir%/}) '$pkg' update success" | log
+                            reboot_flag=1
+                        else
+                            echo "(${dir%/}) $inst_result" | log
+                        fi
+                    #else
+                    #    echo "(${dir%/}) '$pkg' up to date" | log
+                    fi
+                done < $path/$task_file
             fi
         else
             echo "(${dir%/}) $update_result" | log
         fi
     fi
 done
-echo "auto update done!" | log
+if [ "$reboot_flag" -eq "1" ]; then
+    echo "gateways updated - reboot required" | log
+    echo "rebooting in 30s ..." | log
+    #sleep 30
+    #sudo reboot
+else
+    echo "all gateways up to date" | log
+fi
